d8acaa9971f2331a824aaf44cb8335cb3c502ab1
Beem140 <66040233140@udru.ac.th>
2024-12-17 10:13:39 +0700

    Update udf.php

--- udf.php
+++ udf.php
@@ -6,6 +6,19 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>ทดสอบการสร้าง Function ที่สร้างโดยผู้พัฒนาโปรเเกรม</h1>
+    <?php
+        $name = "ใส่ชื่อนามสกุลของตัวเองตรงนี้";
+        Hello($name);
+
+    function Hello($yourname){
+        echo "<h3>ผู้พัฒนาโปรแกรม<h3>";
+        echo "<h3>.$yourname.<h3>";
+        echo "<hr>";
+    }
+    function sum($x,$y){
+        $z = $x+$y;
+        return $z;
+    }
 </body>
 </html>
