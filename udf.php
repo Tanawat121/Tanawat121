f0f17c7d9f008c187fd881545192aa77b573c177
Beem140 <66040233140@udru.ac.th>
2024-12-17 10:37:41 +0700

    Update udf.php

--- udf.php
+++ udf.php
@@ -8,7 +8,7 @@
 <body>
     <h1>ทดสอบการสร้าง Function ที่สร้างโดยผู้พัฒนาโปรเเกรม</h1>
     <?php
-        $name = "ใส่ชื่อนามสกุลของตัวเองตรงนี้";
+        $name = "Tanawat Suwanthong";
         Hello($name);
         //--------------------------------------
         echo "<h3>ทดสอบการใช้ Function</h3>";
@@ -16,10 +16,30 @@
         $b = 8;
         $c = sum($a,$b);
         echo "$a + $b = $c <br>";
+        echo "<hr>";
+        echo "<h3>ทดสอบการใช้ Fumction แบบมีพารามิเตอร์เป็น Reference</h3>";
+        $num = 2;
+        echo "Before ===> \$num = $num <br>";
+        add_5($num);
+        echo "After ===> \$num =$num";
+        echo "<hr>";
+        echo "<h3>ทดสอบการใช้ Function แบบพารามิเตอร์หลายตัว</h3>";
+        $summaryNumber = sumMyNumber(1,2,3,4,5,6,7,8,9);
+        echo "ผลรวมของ (1,2,3,4,5,6,7,8,9) = $summaryNumber";
+        echo "<hr>";
 
         //--------------------------------------
         bye();
-
+    function sumMyNumber(...$x){
+        $sum=0;
+        foreach ($x as $value) {
+            $sum += $value;
+        }
+        return $sum;
+    }
+    function add_5(&$value){
+        $value+5;
+    }
     function Hello($yourname){
         echo "<h3>ผู้พัฒนาโปรแกรม<h3>";
         echo "<h3>.$yourname.<h3>";
